3a69d5c8a0906143d442511f6f40c15a4e11e597
deluxebrain <matt.caton@mac.com>
2023-09-19 16:10:59 +0100

    wip

--- .config/zsh_custom/code.zsh
+++ .config/zsh_custom/code.zsh
@@ -6,3 +6,9 @@
 function code.sync() {
     code --list-extensions | tee "${XDG_CONFIG_HOME}/vscode/extensions"
 }
+
+# installs extensions from dotfiles
+function code.install-extensions() {
+    awk "NF" "$XDG_CONFIG_HOME/vscode/extensions" \
+    | xargs -I{} code --force --install-extension {}
+}
