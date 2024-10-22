790e2ba69a97f7d652c334d7e9b945f8a41fba2f
deluxebrain <matt.caton@mac.com>
2024-10-22 07:25:34 +0100

    wip

--- .config/zsh_custom/gh.zsh
+++ .config/zsh_custom/gh.zsh
@@ -11,30 +11,65 @@ function gh.wrapper() {
     \gh "$@"
 }
 
+# Get the current GitHub token context
+# Usage: gh.current
+function gh.current() {
+    echo "${GH_CURRENT_CONTEXT:-DEFAULT}"
+}
+
+# Use a specific GitHub token context
+# Usage: gh.use <context>
+function gh.use() {
+    local context="$1"
+    if [ -z "$context" ]; then
+        context="DEFAULT"
+    fi
+    GH_CURRENT_CONTEXT="${context:u}"
+    gh.set_auth_token
+}
+
 # pull gh auth token from keychain and set in GITHUB_TOKEN env var
 function gh.set_auth_token() {
+    local context="$(gh.current)"
+    # Construct the token name by uppercasing the context
+    # This allows for multiple GitHub tokens to be stored and retrieved
+    # based on different contexts (e.g., GITHUB_TOKEN_DEFAULT, GITHUB_TOKEN_WORK)
+    local token_name="GITHUB_TOKEN_${context:u}"
     GITHUB_TOKEN=$(\
         security find-generic-password \
             -a "$USER" \
-            -s "GITHUB_TOKEN" \
-            -w 2>/dev/null ; true)
+            -s "$token_name" \
+            -w 2>/dev/null)
+
+    if [ -z "$GITHUB_TOKEN" ]; then
+        echo "Enter github cli auth token for context $context: "
+        read -s GITHUB_TOKEN
+        gh.add_token_to_keychain "$GITHUB_TOKEN" "$token_name"
+    fi
 }
 
 # add github token to keychain
 # pulls token from first parameter or prompts for it if missing
+# secret name can be specified as second parameter, defaults to GITHUB_TOKEN
 function gh.add_token_to_keychain() {
     local token="$1"
-    if [ -z "$token" ] ; then
-        echo "Enter github cli auth token: "
-        read token
+    local token_name="$2"
+
+    if [ -z "$token" ]; then
+        echo "Error: Token is required."
+        return 1
+    fi
+
+    if [ -z "$token_name" ]; then
+        echo "Error: Token name is required."
+        return 1
     fi
     # use -T "" to prevent security application from being allowed
     # passwordless retrieval of the token
     security add-generic-password \
         -a "$USER" \
-        -s "GITHUB_TOKEN" \
+        -s "$token_name" \
         -U \
         -T "" \
         -w "$token"
-    GITHUB_TOKEN="$token"
 }
