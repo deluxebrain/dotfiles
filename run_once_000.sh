f939467451be8d8af89ffae17f8b3af0315bbe26
deluxebrain <matt.caton@mac.com>
2024-12-20 16:48:23 +0000

    wip

--- run_once_000.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "************************** HERE **************************"
-echo $XDG_CONFIG_HOME
-echo $XDG_DATA_HOME
-echo $XDG_CACHE_HOME
-echo $XDG_STATE_HOME
-echo "************************** HERE **************************"
