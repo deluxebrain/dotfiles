849703bffb267ec403282398e41ba5de795b425c
deluxebrain <matt.caton@mac.com>
2025-11-03 20:48:01 +0000

    wip/local machine backup

--- dot_config/zsh_custom/ops.zsh
+++ dot_config/zsh_custom/ops.zsh
@@ -6,4 +6,5 @@ function gen_secret() {
 
     # Generate a random alphanumeric string of specified length with an optional prefix
     echo "${PREFIX}$(openssl rand -base64 $((LENGTH * 2)) | tr -dc 'A-Za-z0-9')" | head -c $LENGTH
+    echo
 }
